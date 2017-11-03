e462cc17edc901e8a190673b1bd20487dc350234
Nikitha Ponna <nikitha.ponna@sap.com>
2017-11-02 23:21:18 -0700

    Programming assignment 2

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,8 +17,37 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+h = X * theta;
+hx = sigmoid(h);
+hx_minus = 1 - hx;
+log_hx = log(hx);
+log_hx_minus = log(hx_minus);
+minus_y = - y;
+y_minus = - (1 - y);
+div = 1/m;
 
+mul = minus_y' * log_hx;
+mul2 = y_minus' * log_hx_minus;
+J_before = div * (mul + mul2);
+theta_square = 0;
 
+for j = 2:size (theta)
+  theta_square = theta_square + (theta(j))^2;
+end
+
+div2 = lambda/(2*m);
+
+J = J_before + (div2 * theta_square);
+
+diff = hx - y;
+sum = diff' * X;
+grad = div .* sum;
+
+div3 = lambda/m;
+
+for j = 2:size (theta)
+  grad(j) = grad(j) + div3*theta(j);
+end
 
 
 
